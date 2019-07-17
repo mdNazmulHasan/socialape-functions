78b3b154e765669e895ec992951f4f673bfae271
mdNazmulHasan <najmul.hasan10@gmail.com>
2019-07-17 19:00:43 +0600

    init commit

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+exports.getScreams = functions.https.onRequest((req, res) => {
+  admin.firestore()
+    .collection("screams")
+    .get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push(doc.data());
+      });
+      return res.json(screams);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
