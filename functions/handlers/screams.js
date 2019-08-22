9125246ecfbd3c806422dc95db9862e5e585db9e
nhaucorp <nazmul@springrainit.com>
2019-08-22 13:22:46 +0600

    delete scream

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -184,3 +184,26 @@ exports.unlikeScream = (req, res) => {
       return res.status(500).json({ error: "something went wrong" });
     });
 };
+
+exports.deleteScream = (req, res) => {
+  const document = db.doc(`/screams/${req.params.screamId}`);
+  document
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "scream not found" });
+      }
+      if (doc.data().userHandle !== req.user.handle) {
+        return res.status(403).json({ error: "Unauthorized" });
+      } else {
+        return document.delete();
+      }
+    })
+    .then(() => {
+      res.json({ message: "Scream deleted" });
+    })
+    .catch(error => {
+      console.error(error);
+      return res.status(500).json({ error: "something went wrong" });
+    });
+};
--- functions/index.js
+++ functions/index.js
@@ -7,15 +7,10 @@ const {
   getScream,
   commentOnScream,
   likeScream,
-  unlikeScream
+  unlikeScream,
+  deleteScream
 } = require("./handlers/screams");
-const {
-  signUp,
-  login,
-  uploadImage,
-  addUserDetails,
-  getUserDetails
-} = require("./handlers/user");
+const { signUp, login, uploadImage, addUserDetails, getUserDetails } = require("./handlers/user");
 const FBAuth = require("./util/fbAuth");
 
 //scream routes
@@ -25,6 +20,7 @@ app.get("/scream/:screamId", getScream);
 app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 app.get("/screams/:screamId/like", FBAuth, likeScream);
 app.get("/screams/:screamId/unlike", FBAuth, unlikeScream);
+app.delete("/scream/:screamId", FBAuth, deleteScream);
 
 //user routes
 app.post("/signup", signUp);
