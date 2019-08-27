bc321fcdb751534363f9bf71c63b1c2ab343794c
nhaucorp <nazmul@springrainit.com>
2019-08-27 12:36:52 +0600

    createnotification on like function added,untested

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,7 @@ const {
 } = require("./handlers/screams");
 const { signUp, login, uploadImage, addUserDetails, getUserDetails } = require("./handlers/user");
 const FBAuth = require("./util/fbAuth");
+const { db } = require("./util/admin");
 
 //scream routes
 app.get("/screams", getAllScreams);
@@ -30,3 +31,56 @@ app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getUserDetails);
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore.document("likes/{id}").onCreate(snapshot => {
+  db.doc(`/screams/${snapshot.data().screamId}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        return db
+          .doc(`/notifications/${snapshot.id}`)
+          .set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            screamId: doc.id
+          })
+          .then(() => {
+            return;
+          })
+          .catch(err => {
+            console.error(err);
+            return;
+          });
+      }
+    });
+});
+exports.createNotificationOnComments = functions.firestore
+  .document("comments/{id}")
+  .onCreate(snapshot => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db
+            .doc(`/notifications/${snapshot.id}`)
+            .set({
+              createdAt: new Date().toISOString(),
+              recipient: doc.data().userHandle,
+              sender: snapshot.data().userHandle,
+              type: "like",
+              read: false,
+              screamId: doc.id
+            })
+            .then(() => {
+              return;
+            })
+            .catch(err => {
+              console.error(err);
+              return;
+            });
+        }
+      });
+  });
